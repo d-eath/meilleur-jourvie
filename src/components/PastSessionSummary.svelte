907580cbccb2b70d081c01c9fd18caba34fc6263
Piggered <git@piggered.me>
2022-09-14 03:27:41 -0400

    move session summary to seperate component

---
+++ src/components/PastSessionSummary.svelte
@@ -0,0 +1,108 @@
+<script>
+    import { Column, Grid, Row, Tag } from 'carbon-components-svelte'
+
+    import ChatIcon from 'carbon-icons-svelte/lib/Chat.svelte'
+    import AttachmentIcon from 'carbon-icons-svelte/lib/Attachment.svelte'
+
+    import 'dayjs/locale/fr'
+    import dayjs from 'dayjs'
+
+    export let session
+
+    function calculateCommentsPerHour(session) {
+        const comments = session.comments.filter(c => c.type === 'comment').length 
+        const hours = dayjs(session.timestampEnd).diff(dayjs(session.timestampStart), 'hour', true)
+
+        if (hours === 0) {
+            return 0
+        }
+
+        return (comments / hours).toFixed(1).replace('.', ',')
+    }
+
+    function calculateDuration(timestampStart, timestampEnd) {
+        const fullSeconds = dayjs(timestampEnd).diff(dayjs(timestampStart), 'second')
+
+        const hours = Math.floor(fullSeconds / 3600)
+        const minutes = Math.floor(fullSeconds % 3600 / 60)
+        const seconds = fullSeconds % 60
+
+        let result = ''
+
+        if (hours > 0) {
+            result += hours + 'h '
+        }
+
+        if (minutes > 0) {
+            result += minutes + 'm '
+        }
+
+        result += seconds + 's'
+        
+        return result
+    }
+</script>
+
+<Grid narrow>
+    <Row>
+        <Column>
+            <h6>Tâche</h6>
+        </Column>
+        <Column>
+            <h6>Date de début</h6>
+        </Column>
+        <Column>
+            <h6>Intervalle d'heure</h6>
+        </Column>
+        <Column>
+            <h6>Durée</h6>
+        </Column>
+        <Column>
+            <h6>Commentaires</h6>
+        </Column>
+    </Row>
+    <Row>
+        <Column>
+            <Tag type="blue" size="sm">{session.task.number}</Tag>
+        </Column>
+        <Column>
+            <span>
+                {dayjs(session.timestampStart).locale('fr').format('dddd D MMMM YYYY')}
+            </span>
+        </Column>
+        <Column>
+            <span>
+                {dayjs(session.timestampStart).locale('fr').format('HH:mm')} -
+                {dayjs(session.timestampEnd).locale('fr').format('HH:mm')}
+            </span>
+        </Column>
+        <Column>
+            <span>
+                {calculateDuration(session.timestampStart, session.timestampEnd)}
+            </span>
+        </Column>
+        <Column>
+            <span>
+                <span class="comment-icons"><ChatIcon /></span>
+                {session.comments.filter(c => c.type === 'comment').length}
+                <span class="comment-ratio">({calculateCommentsPerHour(session)}/h)</span> •
+                <span class="comment-icons"><AttachmentIcon /></span>
+                {session.comments.filter(c => c.type === 'file').length}
+            </span>
+        </Column>
+    </Row>
+</Grid>
+
+<style>
+    .comment-icons {
+        vertical-align: middle;
+    }
+
+    .comment-ratio {
+        color: #666;
+    }
+    
+    :global(.bx--tag) {
+        cursor: pointer;
+    }
+</style>
--- src/routes/Journal.svelte
+++ src/routes/Journal.svelte
@@ -1,21 +1,10 @@
 <script>
-    import {
-        Accordion,
-        AccordionItem,
-        Column,
-        Grid,
-        Row,
-        Tag,
-    } from 'carbon-components-svelte'
-
-    import ChatIcon from 'carbon-icons-svelte/lib/Chat.svelte'
-    import AttachmentIcon from 'carbon-icons-svelte/lib/Attachment.svelte'
+    import { Accordion, AccordionItem } from 'carbon-components-svelte'
 
     import Base from '../components/Base.svelte'
+    import PastSessionSummary from '../components/PastSessionSummary.svelte'
 
-    import 'dayjs/locale/fr'
     import axios from 'axios'
-    import dayjs from 'dayjs'
     import { onMount } from 'svelte'
     import { get } from 'svelte/store'
     import { stringify } from 'query-string'
@@ -125,39 +114,6 @@
             session.comments = [...session.comments, newFile]
         }
     }
-
-    function calculateDuration(timestampStart, timestampEnd) {
-        const fullSeconds = dayjs(timestampEnd).diff(dayjs(timestampStart), 'second')
-
-        const hours = Math.floor(fullSeconds / 3600)
-        const minutes = Math.floor(fullSeconds % 3600 / 60)
-        const seconds = fullSeconds % 60
-
-        let result = ''
-
-        if (hours > 0) {
-            result += hours + 'h '
-        }
-
-        if (minutes > 0) {
-            result += minutes + 'm '
-        }
-
-        result += seconds + 's'
-        
-        return result
-    }
-
-    function calculateCommentsPerHour(session) {
-        const comments = session.comments.filter(c => c.type === 'comment').length 
-        const hours = dayjs(session.timestampEnd).diff(dayjs(session.timestampStart), 'hour', true)
-
-        if (hours === 0) {
-            return 0
-        }
-
-        return (comments / hours).toFixed(1).replace('.', ',')
-    }
 </script>
 
 <Base>
@@ -168,57 +124,8 @@
         <Accordion align="start">
             {#each pastSessions as session}
                 <AccordionItem>
-                    <svelte:fragment slot="title">
-                        <Grid narrow>
-                            <Row>
-                                <Column>
-                                    <h6>Tâche</h6>
-                                </Column>
-                                <Column>
-                                    <h6>Date de début</h6>
-                                </Column>
-                                <Column>
-                                    <h6>Intervalle d'heure</h6>
-                                </Column>
-                                <Column>
-                                    <h6>Durée</h6>
-                                </Column>
-                                <Column>
-                                    <h6>Commentaires</h6>
-                                </Column>
-                            </Row>
-                            <Row>
-                                <Column>
-                                    <Tag type="blue" size="sm">{session.task.number}</Tag>
-                                </Column>
-                                <Column>
-                                    <span>
-                                        {dayjs(session.timestampStart).locale('fr').format('dddd D MMMM YYYY')}
-                                    </span>
-                                </Column>
-                                <Column>
-                                    <span>
-                                        {dayjs(session.timestampStart).locale('fr').format('HH:mm')} -
-                                        {dayjs(session.timestampEnd).locale('fr').format('HH:mm')}
-                                    </span>
-                                </Column>
-                                <Column>
-                                    <span>
-                                        {calculateDuration(session.timestampStart, session.timestampEnd)}
-                                    </span>
-                                </Column>
-                                <Column>
-                                    <span>
-                                        <span class="comment-icons"><ChatIcon /></span>
-                                        {session.comments.filter(c => c.type === 'comment').length}
-                                        <span class="comment-ratio">({calculateCommentsPerHour(session)}/h)</span> •
-                                        <span class="comment-icons"><AttachmentIcon /></span>
-                                        {session.comments.filter(c => c.type === 'file').length}
-                                    </span>
-                                </Column>
-                            </Row>
-                        </Grid>
-                    </svelte:fragment>
+                    <PastSessionSummary session={session} slot="title" />
+
                     {#each session.comments as comment}
                         <div>{comment.content}</div>
                     {/each}
@@ -231,17 +138,5 @@
 <style>
     h2 {
         margin-bottom: 24px;
-    }
-
-    .comment-icons {
-        vertical-align: middle;
-    }
-
-    .comment-ratio {
-        color: #666;
-    }
-
-    :global(.bx--tag) {
-        cursor: pointer;
     }
 </style>
