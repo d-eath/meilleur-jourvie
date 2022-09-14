360caf9d94072d3a3162777575b88e4c30c3a230
Piggered <git@piggered.me>
2022-09-14 00:29:20 -0400

    seperate current session and past sessions

--- src/routes/Journal.svelte
+++ src/routes/Journal.svelte
@@ -10,8 +10,6 @@
 
     import ChatIcon from 'carbon-icons-svelte/lib/Chat.svelte'
     import AttachmentIcon from 'carbon-icons-svelte/lib/Attachment.svelte'
-    import PlayIcon from 'carbon-icons-svelte/lib/Play.svelte'
-    import StopIcon from 'carbon-icons-svelte/lib/Stop.svelte'
 
     import Base from '../components/Base.svelte'
 
@@ -25,41 +23,46 @@
 
     const { VITE_API_URL } = import.meta.env
 
-    let sessions = []
-
-    const statusTooltips = {
-        sessionStart: { icon: PlayIcon, text: 'Début d\'une session', color: 'green' },
-        sessionEnd: { icon: StopIcon, text: 'Fin d\'une session', color: 'red' },
-        comment: { icon: ChatIcon, text: 'Commentaire', color: 'blue' },
-        file: { icon: AttachmentIcon, text: 'Fichier', color: 'gray' }
-    }
+    let currentSession = null
+    let pastSessions = []
 
     onMount(async () => {
         await getSessions()
         await getComments()
         await getFiles()
 
-        for (const session of sessions) {
+        currentSession.comments.sort((a, b) => b.timestamp - a.timestamp)
+
+        for (const session of pastSessions) {
             session.comments.sort((a, b) => b.timestamp - a.timestamp)
         }
 
-        sessions = sessions
+        currentSession = currentSession
+        pastSessions = pastSessions
     })
 
     async function getSessions() {
         const req = await axios.get(`${VITE_API_URL}/getSessionsTravail.php?devId=${get(userInfo).id}`)
 
         for (const session of req.data) {
-            sessions = [...sessions, {
+            const newSession = {
                 id: parseInt(session.Id),
                 timestampStart: new Date(session.Debut).valueOf(),
-                timestampEnd: new Date(session.Fin).valueOf(),
+                timestampEnd: session.Fin ? new Date(session.Fin).valueOf() : null,
                 task: {
                     id: parseInt(session.TacheId),
                     number: session.TacheNumero
                 },
                 comments: []
-            }]
+            }
+
+            if (newSession.timestampEnd === null) {
+                currentSession = newSession
+
+                continue;
+            }
+
+            pastSessions = [...pastSessions, newSession]
         }
     }
 
@@ -73,14 +76,23 @@
         })
 
         for (const comment of req.data) {
-            const session = sessions.find(s => s.id === parseInt(comment.Session_Id))
-
-            session.comments = [...session.comments, {
+            const sessionId = parseInt(comment.Session_Id)
+            const newComment = {
                 type: 'comment',
                 id: parseInt(comment.Id),
                 timestamp: new Date(comment.Horodateur).valueOf(),
                 content: comment.Contenu
-            }]
+            }
+
+            if (sessionId === currentSession?.id) {
+                currentSession.comments = [...currentSession.comments, newComment]
+
+                continue;
+            }
+
+            const session = pastSessions.find(s => s.id === sessionId)
+
+            session.comments = [...session.comments, newComment]
         }
     }
 
@@ -94,16 +106,23 @@
         })
 
         for (const file of req.data) {
-            const timestamp = new Date(file.DateTele).valueOf()
-            const session = sessions.find(s => s.timestampStart <= timestamp && s.timestampEnd >= timestamp)
-
-            session.comments = [...session.comments, {
+            const newFile = {
                 type: 'file',
                 id: parseInt(file.Id),
-                timestamp,
+                timestamp: new Date(file.DateTele).valueOf(),
                 filename: file.NomFichier,
                 mimeType: file.Extension
-            }]
+            }
+
+            if (newFile.timestamp >= currentSession.timestampStart) {
+                currentSession.comments = [...currentSession.comments, newFile]
+
+                continue;
+            }
+
+            const session = pastSessions.find(s => s.timestampStart <= newFile.timestamp && s.timestampEnd >= newFile.timestamp)
+
+            session.comments = [...session.comments, newFile]
         }
     }
 
@@ -112,7 +131,7 @@
 
         const hours = Math.floor(fullSeconds / 3600)
         const minutes = Math.floor(fullSeconds % 3600 / 60)
-        const seconds = Math.floor(fullSeconds % 60)
+        const seconds = fullSeconds % 60
 
         const hDisplay = hours > 0 ? hours + 'h ' : ''
         const mDisplay = minutes > 0 ? minutes + 'm ' : ''
@@ -124,66 +143,68 @@
 <Base>
     <h1>Journal</h1>
 
-    <h2>Sessions précédentes</h2>
-    <Accordion align="start">
-        {#each sessions as session}
-            <AccordionItem>
-                <svelte:fragment slot="title">
-                    <Grid narrow>
-                        <Row>
-                            <Column>
-                                <h6>№ tâche</h6>
-                            </Column>
-                            <Column>
-                                <h6>Date de début</h6>
-                            </Column>
-                            <Column>
-                                <h6>Intervalle d'heure</h6>
-                            </Column>
-                            <Column>
-                                <h6>Durée</h6>
-                            </Column>
-                            <Column>
-                                <h6>Commentaires</h6>
-                            </Column>
-                        </Row>
-                        <Row>
-                            <Column>
-                                <Tag type="blue" size="sm">{session.task.number}</Tag>
-                            </Column>
-                            <Column>
-                                <span>
-                                    {dayjs(session.timestampStart).locale('fr').format('dddd D MMMM YYYY')}
-                                </span>
-                            </Column>
-                            <Column>
-                                <span>
-                                    {dayjs(session.timestampStart).locale('fr').format('HH:mm')} -
-                                    {dayjs(session.timestampEnd).locale('fr').format('HH:mm')}
-                                </span>
-                            </Column>
-                            <Column>
-                                <span>
-                                    {calculateDuration(session.timestampStart, session.timestampEnd)}
-                                </span>
-                            </Column>
-                            <Column>
-                                <span>
-                                    <span class="comment-icons"><ChatIcon /></span>
-                                    {session.comments.filter(c => c.type === 'comment').length} •
-                                    <span class="comment-icons"><AttachmentIcon /></span>
-                                    {session.comments.filter(c => c.type === 'file').length}
-                                </span>
-                            </Column>
-                        </Row>
-                    </Grid>
-                </svelte:fragment>
-                {#each session.comments as comment}
-                    <div>{comment.content}</div>
-                {/each}
-            </AccordionItem>
-        {/each}
-    </Accordion>
+    {#if pastSessions.length > 0}
+        <h2>Sessions précédentes</h2>
+        <Accordion align="start">
+            {#each pastSessions as session}
+                <AccordionItem>
+                    <svelte:fragment slot="title">
+                        <Grid narrow>
+                            <Row>
+                                <Column>
+                                    <h6>№ tâche</h6>
+                                </Column>
+                                <Column>
+                                    <h6>Date de début</h6>
+                                </Column>
+                                <Column>
+                                    <h6>Intervalle d'heure</h6>
+                                </Column>
+                                <Column>
+                                    <h6>Durée</h6>
+                                </Column>
+                                <Column>
+                                    <h6>Commentaires</h6>
+                                </Column>
+                            </Row>
+                            <Row>
+                                <Column>
+                                    <Tag type="blue" size="sm">{session.task.number}</Tag>
+                                </Column>
+                                <Column>
+                                    <span>
+                                        {dayjs(session.timestampStart).locale('fr').format('dddd D MMMM YYYY')}
+                                    </span>
+                                </Column>
+                                <Column>
+                                    <span>
+                                        {dayjs(session.timestampStart).locale('fr').format('HH:mm')} -
+                                        {dayjs(session.timestampEnd).locale('fr').format('HH:mm')}
+                                    </span>
+                                </Column>
+                                <Column>
+                                    <span>
+                                        {calculateDuration(session.timestampStart, session.timestampEnd)}
+                                    </span>
+                                </Column>
+                                <Column>
+                                    <span>
+                                        <span class="comment-icons"><ChatIcon /></span>
+                                        {session.comments.filter(c => c.type === 'comment').length} •
+                                        <span class="comment-icons"><AttachmentIcon /></span>
+                                        {session.comments.filter(c => c.type === 'file').length}
+                                    </span>
+                                </Column>
+                            </Row>
+                        </Grid>
+                    </svelte:fragment>
+                    {#each session.comments as comment}
+                        <div>{comment.content}</div>
+                    {/each}
+                </AccordionItem>
+            {/each}
+        </Accordion>
+    {/if}
 </Base>
 
 <style>
